94b993f354a0dcfbcb65dfe0e1e95169076a1ab2
Michael Jarvis <mjarvis9541@gmail.com>
2024-07-04 13:10:07 +0100

    Remove some elements from App component

--- src/app.rs
+++ src/app.rs
@@ -3,7 +3,6 @@ use leptos_meta::*;
 use leptos_router::*;
 
 use crate::error_template::{AppError, ErrorTemplate};
-
 use crate::page::file_upload_page::FileUploadPage;
 
 #[component]
@@ -19,20 +18,18 @@ pub fn App() -> impl IntoView {
             outside_errors.insert_with_default_key(AppError::NotFound);
             view! { <ErrorTemplate outside_errors/> }.into_view()
         }>
-            <main>
-                <nav class="flex">
-                    <a class="px-4 py-2" href="/">
-                        "Home"
-                    </a>
-                    <a class="px-4 py-2" href="/file-upload">
-                        "File Upload"
-                    </a>
-                </nav>
-                <Routes>
-                    <Route path="" view=HomePage/>
-                    <Route path="/file-upload" view=FileUploadPage/>
-                </Routes>
-            </main>
+            <nav class="flex">
+                <a class="px-4 py-2" href="/">
+                    "Home"
+                </a>
+                <a class="px-4 py-2" href="/file-upload">
+                    "File Upload"
+                </a>
+            </nav>
+            <Routes>
+                <Route path="" view=HomePage/>
+                <Route path="/file-upload" view=FileUploadPage/>
+            </Routes>
         </Router>
     }
 }
